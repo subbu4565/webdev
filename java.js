48e2ba1f2de9ace53aaa4f73b312a5ce8426e219
subbu4565 <subbureddy789@gmail.com>
2024-06-23 09:59:55 -0500

    first commit

--- java.js
+++ java.js
@@ -1,27 +1,10 @@
-const library = {
-    books: [
-        {
-            title: "To Kill a Mockingbird",
-            author: "Harper Lee",
-            year: 1960
-        }
-        
-       
-    ]
-};
-
-// Example of accessing the books
-const newBook = {
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    year: 1813
-};
-
-library.books.push(newBook);
-
-
-console.log(library.books);
-delete library.books;
-console.log(library.books);
-
-
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+    
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
